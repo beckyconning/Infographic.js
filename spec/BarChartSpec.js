3afbadd28172240316c6d11f0553675b5b295a7f
Becky Conning <becky.conning@gmail.com>
2013-01-24 06:43:15 +0000

    Revert to 10c999435729a29a128161dc402a5aa81d711f3e

--- spec/BarChartSpec.js
+++ spec/BarChartSpec.js
@@ -9,10 +9,11 @@ describe("BarChart", function() {
 			text: function() { return { tagName: "text" }; },
 			canvas: { clientWidth: 250, clientHeight: 300 } 
 		};
-		values = [3.1, 4, 2];	
+		values = [3.14252, 4.264322, 2.62694, 1];
 		options = {
 			barSpacingAsWidthMultiplier: 1,
-			valueLabels: true
+			valueLabels: true,
+			orientation: "column"
 		};
 		
 		barChart = BarChart(paper, values, options);
@@ -33,7 +34,7 @@ describe("BarChart", function() {
 		
 		describe("when values is defined", function() {
 		  beforeEach(function() {
-		  	values = [3.1, 4];
+				values = [3.14252, 4.264322, 2.62694, 1];
 		  	barChart = BarChart(paper, values);
 		  });
 		  
@@ -47,6 +48,7 @@ describe("BarChart", function() {
 		  	);
 		  });
 		});
+		
 		
 		describe("when barSpacingAsWidthMultiplier is undefined", function() {
 			beforeEach(function() {
@@ -70,15 +72,26 @@ describe("BarChart", function() {
 				expect(barChart.barSpacingAsWidthMultiplier).toEqual(options.barSpacingAsWidthMultiplier);
 			});
 		});
-    
-		describe("when barSpacingAsWidthMultiplier is defined", function() {
+		
+		describe("when orientation is undefined", function() {
 			beforeEach(function() {
-				options.barSpacingAsWidthMultiplier = 0.25;
+				options.orientation = undefined;
 				barChart = BarChart(paper, values, options);
 			});
     
-			it("should equal the provided barSpacingAsWidthMultiplier", function() {
-				expect(barChart.barSpacingAsWidthMultiplier).toEqual(options.barSpacingAsWidthMultiplier);
+			it("should equal columns", function() {
+				expect(barChart.orientation).toEqual("column");
+			});
+		});
+		
+		describe("when orientation is defined", function() {
+			beforeEach(function() {
+				options.orientation = "row";
+				barChart = BarChart(paper, values, options);
+			});
+    
+			it("should equal given orientation", function() {
+				expect(barChart.orientation).toEqual(options.orientation);
 			});
 		});
 		
@@ -120,17 +133,37 @@ describe("BarChart", function() {
 					expect(paper.text).toHaveBeenCalled();
 				});
 			});
+			
+			describe("when valueLabelsDecimalAccuracy is undefined", function() {
+				beforeEach(function() {
+					options.valueLabelsDecimalAccuracy = undefined;
+					barChart = BarChart(paper, values, options);
+				});
+
+				it("should equal 0.1", function() {
+					expect(barChart.valueLabelsDecimalAccuracy).toEqual(2);
+				});
+			});
+
+			describe("when valueLabelsDecimalAccuracy is defined", function() {
+				beforeEach(function() {
+					options.valueLabelsDecimalAccuracy = 4;
+					barChart = BarChart(paper, values, options);
+				});
+
+				it("should equal the provided barSpacingAsWidthMultiplier", function() {
+					expect(barChart.valueLabelsDecimalAccuracy).toEqual(options.valueLabelsDecimalAccuracy);
+				});
+			});
 
 		});
 	});
 	
 	describe("drawBars()", function() {
 		beforeEach(function() {			
-			values = [3.1, 4, 2];
+			values = [3.14252, 4.264322, 2.62694, 1];
 			barChart = BarChart(paper, values, options);
-			
 			spyOn(paper, "rect");
-			
 			barChart.drawBars();
 		});
 		
@@ -138,19 +171,54 @@ describe("BarChart", function() {
 			expect(paper.rect.calls.length).toEqual(values.length);
 		});
 		
-		it("should draw rects for each value with heights relative to the axis", function() {
-			for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
-				var valueBarHeight = (values[valueIndex] / barChart.yAxisMaxValue) * paperHeight;
-				var valueBarXPosition = ((barWidth + barSpacingWidth) * valueIndex);
-				var valueBarYPosition = paperHeight - valueBarHeight;
-				
-				expect(paper.rect.calls[valueIndex].args).toEqual(
-					[valueBarXPosition, valueBarYPosition, barWidth, valueBarHeight]
-				);
-			};
+		describe("when column orientation", function() {
+			beforeEach(function() {
+				options.orientation = "column";
+				barChart = BarChart(paper, values, options);
+				paper.rect.calls.length = 0;
+				barChart.drawBars();
+			});
+			
+			it("should draw rects for each value with heights relative to the axis", function() {
+				for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
+					var valueBarHeight = (values[valueIndex] / barChart.yAxisMaxValue) * paperHeight;
+					var valueBarXPosition = ((barWidth + barSpacingWidth) * valueIndex);
+					var valueBarYPosition = paperHeight - valueBarHeight;
+
+					expect(paper.rect.calls[valueIndex].args).toEqual(
+						[valueBarXPosition, valueBarYPosition, barWidth, valueBarHeight]
+					);
+				};
+			});
+ 
 		});
 		
-		// elements has undefined in during spec but not elsewhere?
+		describe("when row orientation", function() {
+			var barHeight;
+			beforeEach(function() {
+				options.orientation = "row";
+				barChart = BarChart(paper, values, options);
+				paper.rect.calls.length = 0;
+				barChart.drawBars();
+				barHeight = barWidth;
+				barSpacingHeight = barSpacingWidth;
+			});
+			
+			it("should draw rects for each value with widths relative to the axis", function() {
+				for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
+					var valueBarWidth = (values[valueIndex] / barChart.yAxisMaxValue) * paperWidth;
+					var valueBarXPosition = 0;
+					var valueBarYPosition = ((barHeight + barSpacingHeight) * valueIndex);
+
+					expect(paper.rect.calls[valueIndex].args).toEqual(
+						[valueBarXPosition, valueBarYPosition, valueBarWidth, barHeight]
+					);
+				};
+			});
+ 
+		});
+		
+		// elements contains undefineds in spec but not elsewhere :x
 		it("should put a rect into elements for each value", function() {
 			var rects = barChart.elements.filter(function(element) {
 				if (element) { return (element.tagName == "rect"); }
@@ -164,7 +232,7 @@ describe("BarChart", function() {
 	
 	describe("drawValueLabels()", function() {
 		beforeEach(function() {
-			values = [3.1, 4, 2];
+			values = [3.14252, 4.264322, 2.62694, 1];
 			barChart = BarChart(paper, values, options);
 			
 			spyOn(paper, "text");
@@ -175,22 +243,65 @@ describe("BarChart", function() {
 			expect(paper.text.calls.length).toEqual(values.length);
 		});
 		
-		it("should draw a text field with the value in the center of the bar for each value", function() {
-			for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
-				var valueBarHeight = (values[valueIndex] / barChart.yAxisMaxValue) * paperHeight;
-				var valueBarXPosition = ((barWidth + barSpacingWidth) * valueIndex);
-				var valueBarYPosition = paperHeight - valueBarHeight;
-				
-				var valueLabelXPosition = valueBarXPosition + (barWidth / 2);
-				var valueLabelYPosition = valueBarYPosition + (valueBarHeight / 2);
-				
-				expect(paper.text.calls[valueIndex].args).toEqual(
-					[valueLabelXPosition, valueLabelYPosition, values[valueIndex]]
-				);
-			};
+		describe("when column orientation", function() {
+			beforeEach(function() {
+				options.orientation = "column";
+				barChart = BarChart(paper, values, options);
+				barChart.drawBars();
+			});
+			
+			it("should draw a text field with the value in the center of the bar for each value", function() {
+				for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
+					var barHeight = (values[valueIndex] / barChart.yAxisMaxValue) * paperHeight;
+					var barXPosition = ((barWidth + barSpacingWidth) * valueIndex);
+					var barYPosition = paperHeight - barHeight;
+
+					var labelXPosition = barXPosition + (barWidth / 2);
+					var labelYPosition = barYPosition + (barHeight / 2);
+
+					var valueBeforePoint = values[valueIndex].toString().split(".")[0];
+					var labelPrecision = barChart.valueLabelsDecimalAccuracy + valueBeforePoint.length;
+					var labelText = values[valueIndex].toPrecision(labelPrecision);
+
+					expect(paper.text.calls[valueIndex].args).toEqual(
+						[labelXPosition, labelYPosition, labelText]
+					);
+				};
+			});
+ 
 		});
 		
-		// elements has undefined in during spec but not elsewhere?
+		describe("when row orientation", function() {
+			var barHeight;
+			beforeEach(function() {
+				options.orientation = "row";
+				barChart = BarChart(paper, values, options);
+				barChart.drawBars();
+				barHeight = barWidth;
+				barSpacingHeight = barSpacingWidth;
+			});
+			
+			it("should draw a text field with the value in the center of the bar for each value", function() {
+				for(var valueIndex = 0; valueIndex < values.length; valueIndex ++) {
+					var valueBarWidth = (values[valueIndex] / self.yAxisMaxValue) * paperWidth;
+					var valueBarXPosition = 0;
+					var valueBarYPosition = ((barHeight + barSpacingHeight) * valueIndex);
+          var 
+					var labelXPosition = valueBarXPosition + (valueBarWidth / 2);
+					var labelYPosition = valueBarYPosition + (barHeight / 2);
+
+					var valueBeforePoint = values[valueIndex].toString().split(".")[0];
+					var labelPrecision = barChart.valueLabelsDecimalAccuracy + valueBeforePoint.length;
+					var labelText = values[valueIndex].toPrecision(labelPrecision);
+
+					expect(paper.text.calls[valueIndex].args).toEqual(
+						[labelXPosition, labelYPosition, labelText]
+					);
+				};
+			});
+		});
+		
+		// elements has undefined in during spec but not elsewhere
 		it("should put a text field into elements for each value", function() {
 			var texts = barChart.elements.filter(function(element) {
 				if (element) { return (element.tagName == "text"); }
